06fe8507089409d848d2a8cf0fac456bae1f3925
Henrique Negri <144740293+henriquenegri@users.noreply.github.com>
2024-03-22 21:30:13 -0300

    Create estru_for.java

---
+++ estru_for.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Exercicios {
+   
+    public static void main(String[] args) {
+        // Cria um objeto Scanner para entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Numero: " + i);
+            }
+           
+        scanner.close();
+    }
+}
