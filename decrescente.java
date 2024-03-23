3ee3fcb9627eb7db3f4ab5b85a7e33cd0327f87d
Henrique Negri <144740293+henriquenegri@users.noreply.github.com>
2024-03-22 21:32:42 -0300

    Create decrescente.java

---
+++ decrescente.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicios {
+   
+    public static void main(String[] args) {
+        // Cria um objeto Scanner para entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 10; i>=0; i--){
+        System.out.println("R: "+i);}
+        scanner.close();
+    }
+}
